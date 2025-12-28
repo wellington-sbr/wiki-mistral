dc7f4bca7d1c05e6f4db1924789589a60aefeb28
Wellington St. Báez R. <127298425+wellington-sbr@users.noreply.github.com>
2025-12-28 03:42:08 +0100

    Refactor MediaWiki initialization script
    Removed the creation of LocalSettings.php and added checks for its existence. Updated MediaWiki installation steps and permissions.

--- init-mediawiki.sh
+++ init-mediawiki.sh
@@ -3,58 +3,33 @@ set -e
 
 echo "Initializing MediaWiki..."
 
-# Crear LocalSettings.php de cero
-cat > /var/www/html/LocalSettings.php <<'EOFPHP'
-<?php
-# This file was automatically generated, do not edit by hand
+# Esperar a que la BD esté lista
+sleep 2
 
-if ( !defined( 'MEDIAWIKI' ) ) {
-	exit;
-}
+# Ejecutar MediaWiki install
+cd /var/www/html
+php maintenance/install.php \
+  --dbtype=postgres \
+  --dbserver="ep-odd-queen-ag68f36e-pooler.c-2.eu-central-1.aws.neon.tech" \
+  --dbname="mediawiki_db" \
+  --dbuser="neondb_owner" \
+  --dbpass="npg_p75cgGwkZAxo" \
+  --dbport=5432 \
+  --installdbuser="neondb_owner" \
+  --installdbpass="npg_p75cgGwkZAxo" \
+  --scriptpath="/w" \
+  --lang="es" \
+  --pass="admin123" \
+  "Mistral Wiki" "admin"
 
-$wgSitename = "Mistral Wiki";
-$wgMetaNamespace = "Wiki";
-$wgLanguageCode = "es";
+echo "MediaWiki installation complete"
 
-# Uploaded file restrictions
-$wgEnableUploads = true;
-$wgUploadPath = "$wgScriptPath/images";
-$wgUploadDirectory = "$IP/images";
-
-# Database
-$wgDBtype = "postgres";
-$wgDBserver = "ep-odd-queen-ag68f36e-pooler.c-2.eu-central-1.aws.neon.tech";
-$wgDBname = "mediawiki_db";
-$wgDBuser = "neondb_owner";
-$wgDBpassword = "npg_p75cgGwkZAxo";
-$wgDBport = 5432;
-$wgDBssl = true;
-
-# Server config
-$wgServer = "https://mediawiki-mistral.onrender.com";
-$wgScriptPath = "/w";
-$wgArticlePath = "/wiki/$1";
-
-# Skin
-wfLoadSkin( 'Vector' );
-
-# Security keys
-$wgSecretKey = "d41d8cd98f00b204e9800998ecf8427e";
-$wgUpgradeKey = "d41d8cd98f00b204e9800998ecf8427e";
-
-# Disable most things
-$wgMainCacheType = CACHE_NONE;
-
-$wgGroupPermissions['*']['read'] = true;
-$wgGroupPermissions['*']['edit'] = false;
-$wgGroupPermissions['user']['edit'] = true;
-
-?>
-EOFPHP
-
-echo "LocalSettings.php created"
-chmod 644 /var/www/html/LocalSettings.php
-chown www-data:www-data /var/www/html/LocalSettings.php
+# Copiar el LocalSettings.php generado
+if [ -f "/var/www/html/LocalSettings.php" ]; then
+    echo "LocalSettings.php found"
+    chmod 644 /var/www/html/LocalSettings.php
+    chown www-data:www-data /var/www/html/LocalSettings.php
+fi
 
 echo "Starting Apache..."
 exec apache2-foreground
