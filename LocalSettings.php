b39c51cd0061672955db6e2447469d2b4fd01dea
Wellington St. Báez R. <127298425+wellington-sbr@users.noreply.github.com>
2025-12-28 02:45:30 +0100

    Update database name and server configuration

--- LocalSettings.php
+++ LocalSettings.php
@@ -6,14 +6,14 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 $wgDBtype = "postgres";
 $wgDBserver = "ep-odd-queen-ag68f36e-pooler.c-2.eu-central-1.aws.neon.tech";
 $wgDBport = 5432;
-$wgDBname = "mediawiki_db";
+$wgDBname = "neondb";
 $wgDBuser = "neondb_owner";
 $wgDBpassword = "npg_p75cgGwkZAxo";  
 
 $wgDBssl = true;
 $wgDBsslmode = 'require';
 
-$wgServer = "https://mediawiki-mistral.onrender.com";
+$wgServer = "mediawiki-mistral.onrender.com";
 $wgScriptPath = "/w";
 
 $wgSitename = "Mistral Wiki";
